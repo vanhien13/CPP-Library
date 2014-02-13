1159a37583b8c5bad7d4161d1a020ef1762c0f84
vanhien13 <vanhien13@gmail.com>
2014-02-13 11:44:14 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+   title:       Main.cpp
+   description: a simple main() to jump start the 
+                application...
+*/
+
+#ifndef BORROWBOOKLISTUI
+#include "BorrowBookListUI.h"
+#endif
+
+int main()
+{
+	BorrowBookListUI Library;
+}
